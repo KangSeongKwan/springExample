cecd83ed6c6cfce763f104f1fbfab244a6c5983a
KangSeongKwan <99636945+KangSeongKwan@users.noreply.github.com>
2024-12-05 15:05:18 +0900

    Update EmpAPIController.java
    충돌사항들 반영 후 Commit - Controller

--- src/main/java/app/api/controller/EmpAPIController.java
+++ src/main/java/app/api/controller/EmpAPIController.java
@@ -24,6 +24,25 @@ import lombok.RequiredArgsConstructor;
 public class EmpAPIController {
     private final EmpService empService;
 
+    // 사원 전체 조회
+	@GetMapping("/emps")
+	public List<Emp> getEmps() {
+		List<Emp> emp = empRepository.findAll();
+	
+			if (emp.isEmpty()) {
+			 throw new EntityNotFoundException("사원정보가 존재하지 않습니다.");
+		    }
+	
+		return emp;
+	}
+
+    // 특정 사원 조회
+	@GetMapping("/emp/{empno}")
+	public Emp getEmpByEmpno(@PathVariable Integer empno) {
+		Emp emp = empService.getEmpByEmpno(empno);
+		return emp;
+	}
+	
     // 사원 추가
 	  @PostMapping("/emp")
 	  public Emp registerEmp(@RequestBody Emp newEmp) {
@@ -31,6 +50,22 @@ public class EmpAPIController {
 		      return emp;
 	      }
     }
+
+    // 사원 정보 수정
+    // empno를 PathVariable로 받아서 동적으로 처리
+    @PutMapping("/emp/{empno}")
+    public ResponseEntity<Emp> updateEmployee(@PathVariable int empno, @RequestBody Emp emp) {
+        // empno를 기준으로 해당 직원 정보를 업데이트
+        Emp updatedEmp = empService.updateEmployee(empno, emp);
+
+        if (updatedEmp != null) {
+            return ResponseEntity.ok(updatedEmp); // 성공적으로 업데이트된 직원 반환
+        } else {
+            return ResponseEntity.status(404).body(null); // 직원이 없으면 404 반환
+        }
+    }
+
+    // 사원 정보 삭제
     @DeleteMapping("/emp/{empno}")
     public Emp deleteEmp(@PathVariable("empno") int empno) {
         Emp emp = empRepository.findById(empno).orElseThrow(() -> 
