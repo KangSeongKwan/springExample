3110884f3839f9c6321a5ec85843f1bfb27f6878
KangSeongKwan <99636945+KangSeongKwan@users.noreply.github.com>
2024-12-05 15:10:48 +0900

    Update EmpService.java
    Conflict 사항 수정 반영 후 Commit - Service

--- src/main/java/app/service/EmpService.java
+++ src/main/java/app/service/EmpService.java
@@ -18,9 +18,36 @@ import lombok.RequiredArgsConstructor;
 public class EmpService {
 
 	private final EmpRepository EmpRepository;
+
 	
+	// 특정 사원 정보 조회
+	@Transactional
+	public Emp getEmpByEmpno(Integer empno) {
+		return empRepository.findById(empno).orElseThrow(() -> new EntityNotFoundException(" msg :사원정보가 존재하지 않습니다."));
+	}
+	
+	// 사원 생성
 	@Transactional
 	public Emp registerEmp(Emp newEmp) {
 		return EmpRepository.save(newEmp);
 	}
+
+	// 사원 수정
+        public Emp updateEmployee(int empno, Emp updatedEmp) {
+	    Emp existingEmp = empRepository.findById(empno).orElse(null);
+
+	    if (existingEmp != null) {
+	        existingEmp.setEname(updatedEmp.getEname());
+	        existingEmp.setJob(updatedEmp.getJob());
+	        existingEmp.setMgr(updatedEmp.getMgr());
+	        existingEmp.setHiredate(updatedEmp.getHiredate());
+	        existingEmp.setSal(updatedEmp.getSal());
+	        existingEmp.setComm(updatedEmp.getComm());
+	        existingEmp.setDeptno(updatedEmp.getDeptno());
+
+	        return empRepository.save(existingEmp); // 직원 정보 업데이트
+	    } else {
+	        return null; // 직원이 없다면 null 반환
+	    }
+        }
 }
